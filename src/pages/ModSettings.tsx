65bac6ebc22059d77d0f5e7e45563b9a7295097a
nicklatkovich <nicklatkovich@gmail.com>
2026-09-19 23:29:39 +0300

    Add linux, proton and mac paths to mod settings page

--- src/pages/ModSettings.tsx
+++ src/pages/ModSettings.tsx
@@ -7,8 +7,12 @@ import "../styles/mod-settings.scss";
 
 const WS_DEFAULT_PATH = "C:\\Program Files (x86)\\Steam\\steamapps\\workshop\\content\\341800\\";
 const MS_DEFAULT_PATH = "%AppData%\\..\\LocalLow\\Steel Crate Games\\Keep Talking and Nobody Explodes\\modSettings.xml";
-const LN_WS_DEFAULT_PATH = "~/.local/share/Steam/steamapps/workshop/content/341800/";
+const LN_WS_DEFAULT_PATH = "/home/USERNAME/.local/share/Steam/steamapps/workshop/content/341800/";
 const LN_MS_DEFAULT_PATH = "~/.config/unity3d/Steel Crate Games/Keep Talking and Nobody Explodes/modSettings.xml";
+const SD_WS_DEFAULT_PATH = "Z:\\home\\deck\\.local\\share\\Steam\\steamapps\\workshop\\content\\341800";
+const SD_MS_DEFAULT_PATH = "/home/deck/.steam/steam/steamapps/compatdata/341800/pfx/drive_c/users/steamuser/AppData/LocalLow/Steel Crate Games/Keep Talking and Nobody Explodes/modSettings.xml";
+const MC_WS_DEFAULT_PATH = "/User/USERNAME/Library/Application Support/Steam/steamapps/workshop/content/341800";
+const MC_MS_DEFAULT_PATH = "~/Library/Application Support/com.steelcrategames.keeptalkingandnobodyexplodes/modSettings.xml";
 
 export const ModSettingsPage: React.FC = () => {
   const repoStatus = useAppSelector(repoSelectors.getStatus);
@@ -47,10 +51,10 @@ export const ModSettingsPage: React.FC = () => {
             const fixedPath = path.endsWith("/") || path.endsWith("\\") ? path : (
               path + (path.includes("\\") ? "\\" : "/")
             );
-            for (const mod of Object.values(allModulesState)) {
-              if (!mod.SteamID || (mod.Type !== "Regular" && mod.Type !== "Needy")) continue;
-              result.push(`    <string>${fixedPath}${mod.SteamID}</string>`);
-            }
+            const steamIds = new Set(Object.values(allModulesState)
+              .filter((mod) => mod.SteamID && (mod.Type === "Regular" || mod.Type === "Needy"))
+              .map((mod) => mod.SteamID));
+            result.push(...Array.from(steamIds).map((id) => `    <string>${fixedPath}${id}</string>`));
             result.push(`  </DisabledModPaths>`);
             result.push(`</ModSettings>`);
             result.push("");
@@ -68,8 +72,32 @@ export const ModSettingsPage: React.FC = () => {
           </li>
           <li>Linux:
             <ul>
-              <li>Workshop: <input className="path" type="text" disabled value={LN_WS_DEFAULT_PATH} /></li>
+              <li>
+                Workshop: KTaNE does not accept short paths for <code>modSettings.xml</code> on Linux.
+                Replace <b>USERNAME</b> with your actual system username.
+                You can get it using command `<code>whoami</code>`.
+                <br/>
+                <input className="path" type="text" disabled value={LN_WS_DEFAULT_PATH} />
+              </li>
               <li>modSettings.xml: <input className="path" type="text" disabled value={LN_MS_DEFAULT_PATH} /></li>
+            </ul>
+          </li>
+          <li>Steam Deck (Proton):
+            <ul>
+              <li>Workshop: <input className="path" type="text" disabled value={SD_WS_DEFAULT_PATH} /></li>
+              <li>modSettings.xml: <input className="path" type="text" disabled value={SD_MS_DEFAULT_PATH} /></li>
+            </ul>
+          </li>
+          <li>Mac:
+            <ul>
+              <li>
+                Workshop: KTaNE does not accept short paths for <code>modSettings.xml</code> on Mac.
+                Replace <b>USERNAME</b> with your actual system username.
+                You can get it using command `<code>id -un</code>`.
+                <br/>
+                <input className="path" type="text" disabled value={MC_WS_DEFAULT_PATH} />
+              </li>
+              <li>modSettings.xml: <input className="path" type="text" disabled value={MC_MS_DEFAULT_PATH} /></li>
             </ul>
           </li>
         </ul>
